0f3ba5a2ff2b46792ff41f5b82a6ded6f4d3e63e
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-14 21:21:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,24 +1,16 @@
 def using_concat (array, array2)
 
-array.concat(array2)
-puts array.concat(array2) 
+(array).concat(array2)
 end
 
-my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-more_favs = ["sports cars", "flatiron school"]
 
-using_concat(my_favorite_things, more_favs)
 
 
 
 def using_insert (array, element)
-  puts array.insert(3, (element))
+ (array).insert(element)
 end
 
-  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  another_language = "Python"
-  
-  using_insert(list_of_programming_languages, another_language)
   
   
   def using_uniq (array)
